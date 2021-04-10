35620a4122de0fb4a594797743567ec4aa008cd2
Alexandrov911 <sashok17062002@mail.ru>
2021-04-10 10:07:16 +0300

    1.15

---
+++ 1.15/1.15/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1._15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("введите число a");
+            double a = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("введите число b");
+            double b = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("введите число c");
+            double c = Convert.ToDouble(Console.ReadLine());
+
+
+            double a2 = a / (b * c);
+            Console.WriteLine("ответ:" + a2);
+            Console.ReadKey();
+            
+        }
+    }
+}
